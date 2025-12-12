97dea45f4baebed08982d862ac57416745309de7
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2025-12-12 17:25:37 -0300

    dieedi

--- src/services/googleService.ts
+++ src/services/googleService.ts
@@ -229,8 +229,6 @@ export const getAuthUrl = (whatsappId: string) => {
     prompt: "consent",
     scope: [
       "https://www.googleapis.com/auth/calendar",
-      "https://www.googleapis.com/auth/gmail.readonly",
-      "https://www.googleapis.com/auth/drive",
       "https://www.googleapis.com/auth/documents",
       "https://www.googleapis.com/auth/spreadsheets",
     ],
