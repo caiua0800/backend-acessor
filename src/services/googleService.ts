38d2066941169182447a0610c745950b8f214e72
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2025-12-12 00:03:16 -0300

    emdek

--- src/services/googleService.ts
+++ src/services/googleService.ts
@@ -38,7 +38,6 @@ export const getGoogleAuthUrlRegistration = (state: string) => {
     scope: [
       "https://www.googleapis.com/auth/calendar",
       "https://www.googleapis.com/auth/gmail.readonly",
-      "https://www.googleapis.com/auth/drive",
       "https://www.googleapis.com/auth/documents",
       "https://www.googleapis.com/auth/spreadsheets",
     ],
